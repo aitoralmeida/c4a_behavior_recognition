d7d8fd6df3b9de3bb2839d1dbac86502d7568a96
Aitor Almeida <aitoralmeida@gmail.com>
2017-04-11 09:17:51 +0200

    better evaluation

--- behavior_model_parallel_LSTM.py
+++ behavior_model_parallel_LSTM.py
@@ -316,6 +316,24 @@ def main(argv):
     metrics = model.evaluate([X_actions_test, X_times_test], y_test, batch_size=BATCH_SIZE)
     print metrics
     
+    predictions = model.predict([X_actions_test, X_times_test], BATCH_SIZE)
+    correct = [0] * 5
+    prediction_range = 5
+    for i, prediction in enumerate(predictions):
+        correct_answer = y_test[i].tolist().index(1)       
+        best_n = np.sort(prediction)[::-1][:prediction_range]
+        for j in range(prediction_range):
+            if prediction.tolist().index(best_n[j]) == correct_answer:
+                for k in range(j,prediction_range):
+                    correct[k] += 1 
+    
+    accuracies = []                   
+    for i in range(prediction_range):
+        print '%s prediction accuracy: %s' % (i+1, (correct[i] * 1.0) / len(y_test))
+        accuracies.append((correct[i] * 1.0) / len(y_test))
+    
+    print accuracies
+    
     print '************ FIN ************\n' * 3  
 
 
